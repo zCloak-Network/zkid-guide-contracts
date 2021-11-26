204028f457f5b842ea34567a0084a33ea0b958ab
Bayek <71303553+B6-JohnFack@users.noreply.github.com>
2021-11-26 20:46:12 +0800

    interface class using

--- contracts/interfaces/IRegistry.sol
+++ contracts/interfaces/IRegistry.sol
@@ -1,3 +1,13 @@
+///
+/// @dev 改合约为一个接口类合约，在该合约中，定义了很多可供实现的函数
+/// 接口类合约还对应一个实现类合约，该接口类合约（IRegistry.sol）的实现类合约为 Registry.sol，在实现类合约中实现了接口类合约中的函数
+///
+/// 用法：
+/// IRegistry public registry;
+/// registry = IRegistry(Registry实现类合约的地址)；
+/// registry.stringOf("propertyName");
+///
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
