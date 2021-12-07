0f8b584ef5585663c7982bf28b340043d9736fc2
vsszhang <vsszhang@gmail.com>
2021-12-07 14:09:43 +0800

    update .gitignore and hardhat.config.js about privateKey

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,6 +13,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+// If you have question about secrets.json, please connect to this link:
+// https://docs.moonbeam.network/builders/interact/hardhat/
+const { privateKeyKovan, privateKeyMoonbase } = require('./secrets.json');
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -30,7 +34,19 @@ module.exports = {
   networks: {
     kovan: {
       url: `https://kovan.infura.io/v3/4e9ee595de8f47c7b9997ce895cf6694`,
-      accounts: ["1f2408543a2e34321f989e68306888e04d45962c3463a0771240754930fd9896"]
-    }
+      accounts: [privateKeyKovan]
+    },
+
+    moonbase: {
+      url: 'https://rpc.testnet.moonbeam.network',
+      chainId: 1287, // (hex: 0x507),
+      accounts: [privateKeyMoonbase] // Insert your private key here
+    },
+
+    moonriver: {
+      url: 'https://rpc.moonriver.moonbeam.network',
+      chainId: 1285, //(hex: 0x505),
+      //accounts: [privateKeyMoonriver] // Insert your private key here
+    },
   }
 };
