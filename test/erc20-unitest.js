2c502fb8522d98a153a9c9d1b3354cad945eebd8
vsszhang <vsszhang@gmail.com>
2021-12-23 11:41:12 +0800

    update test script for erc20-unitest

--- test/erc20-unitest.js
+++ test/erc20-unitest.js
@@ -1,6 +1,7 @@
-const { assert,expect } = require("chai");
+const { assert, expect } = require("chai");
 const { ethers } = require("hardhat");
 
+// the COIN's decimal is 18
 const COIN = ethers.BigNumber.from("0xde0b6b3a7640000");
 
 describe("SampleToken", function () {
@@ -24,24 +25,33 @@ describe("SampleToken", function () {
   it("Transfer should work", async function () {
     let user1Balance = await this.token.balanceOf(this.user1.address);
     await this.token.connect(this.owner).transfer(this.user1.address, COIN);
-    
+
     // owner's balance should be (100 - 1) COIN
     assert(await this.token.balanceOf(this.owner.address), COIN.mul(100 - 1));
     assert(await this.token.balanceOf(this.user1.address), user1Balance.add(COIN));
-    
+
   })
 
-  it("TransferFrom shoul work", async function() {
-    // TODO:
+  it("Approve and allowance should work", async function () {
+    var amount = await ethers.utils.parseEther("10.0");
+    await this.token.approve(this.user1.address, amount);
+    // user2's allowance should be 10 USDT
+    expect(await this.token.allowance(this.owner.address, this.user1.address)).to.equal(await ethers.utils.parseEther("10.0"));
   })
 
+  it("TransferFrom should work", async function () {
+    var amount = await ethers.utils.parseEther("10.0");
+    await this.token.approve(this.user1.address, amount);
+    // user1's allowance should be 10 USDT
+    expect(await this.token.allowance(this.owner.address, this.user1.address)).to.equal(await ethers.utils.parseEther("10.0"));
 
-  it("Transferred amount exceeds balance should fail", async function() {
-    // TODO:
-  })
-
-  it("TransferFrom amount exceeds balance should fail", async function() {
-    // TODO:
+    // user1 call function transferFrom()
+    var transferAmount = await ethers.utils.parseEther("1.0");
+    await this.token.connect(this.user1).transferFrom(this.owner.address, this.user1.address, transferAmount);
+    // user1's allowance should be 9 USDT
+    expect(await this.token.allowance(this.owner.address, this.user1.address)).to.equal(await ethers.utils.parseEther("9.0"));
+    // user1's balance should be 1 USDT
+    expect(await this.token.balanceOf(this.user1.address)).to.equal(await ethers.utils.parseEther("1.0"));
   })
 
 });
