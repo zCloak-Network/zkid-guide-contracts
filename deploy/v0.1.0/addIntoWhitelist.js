91dde19d03fcfdb2df580ffda3d51e9e90e6cef7
vsszhang <vsszhang@gmail.com>
2022-06-20 10:22:57 +0800

    add new keeper into whitelist

--- deploy/v0.1.0/addIntoWhitelist.js
+++ deploy/v0.1.0/addIntoWhitelist.js
@@ -1,22 +1,27 @@
 const { ethers } = require("hardhat");
 
 // aggregator auth
-const {addrSAggregatorAuth} = require("../scripts/contract.json");
-
+const {addrSAggregatorAuth} = require("../../tmp/contract.json");
 
 async function main() {
     // only apply to local status
-    const keeper1 = await ethers.getSigner(3);
-    const keeper2 = await ethers.getSigner(4);
+    const keeper4 = await ethers.getSigner(9);
+    const keeper5 = await ethers.getSigner(10);
+    const keeper6 = await ethers.getSigner(11);
+
+    console.log(`keeper4: ${keeper4.address}`);
+    console.log(`keeper5: ${keeper5.address}`);
+    console.log(`keeper6: ${keeper6.address}`);
 
     const Whitelist = await ethers.getContractFactory("SimpleAggregatorAuth");
-    const whitelist = await Whitelist.attach(addrSAggregatorAuth);
+    const whitelist = Whitelist.attach(addrSAggregatorAuth);
 
-    
-    await whitelist.addWorker(keeper1.address);
-    await whitelist.addWorker(keeper2.address)
-    console.log("is keeper1 granted? ", await whitelist.isWorker(keeper1.address));
-    console.log("is keeper2 granted? ", await whitelist.isWorker(keeper2.address));
+    await whitelist.addWorker(keeper4.address);
+    await whitelist.addWorker(keeper5.address);
+    await whitelist.addWorker(keeper6.address);
+    console.log("is keeper4 granted? ", await whitelist.isWorker(keeper4.address));
+    console.log("is keeper5 granted? ", await whitelist.isWorker(keeper5.address));
+    console.log("is keeper6 granted? ", await whitelist.isWorker(keeper6.address));
     // you can add other characters as worker
 }
 
